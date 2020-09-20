b69552a334b98ac139ebe29266ac78bcd4688259
minamAn <minamhunmin@gmail.com>
2020-09-20 18:05:41 +0900

    add leftnav

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "Components/Header";
+import LeftNav from "Components/LeftNav";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <LeftNav />
     </div>
   );
 }
---
+++ src/Components/LeftNav/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftNav = () => {
+  return <div>이것은 레프트</div>;
+};
+
+export default LeftNav;
